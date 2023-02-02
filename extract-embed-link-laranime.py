04673579ed1a7e215f8c07e4d9f822c6067561f8
shwzr <113097122+shwzr@users.noreply.github.com>
2023-02-03 00:16:15 +0100

    Update extract-embed-link-laranime.py

--- extract-embed-link-laranime.py
+++ extract-embed-link-laranime.py
@@ -2,38 +2,26 @@ import requests
 import re
 import os
 
-# Il verifie si le fichier "Episodes.txt" existe et il le supprime si c'est le cas.
-if os.path.exists("Episodes.txt"):
-    os.remove("Episodes.txt")
+# Supprime les fichiers s'ils existent
+for i in range(1, 3):
+    file_path = f"{i}-Episodes.txt"
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
-# Obtiens l'URL de l'utilisateur
-url = input("Entrez un URL laranime.tv que tu peux retrouver pour l'Episode x d'un anime : ")
+# Demande l'URL à l'utilisateur
+url = input("Entrez un URL laranime.tv pour un épisode d'un anime : ")
 
-# Telecharge le code source de la page
+# Récupère le code source de la page
 response = requests.get(url)
+text = response.text.replace("\\", "")
 
-# Il ecrit le code source de la page dans le fichier
-with open("source_code.html", "w", encoding="utf-8") as f:
-    f.write(response.text)
-
-# Il lit le code source de la page depuis le fichier
-with open("source_code.html", "r", encoding="utf-8") as f:
-    text = f.read()
-
-# Il remplace les caracteres d'echappement
-text = text.replace("\\", "")
-
-# Il ecrit le code source de la page reformatee dans le fichier
-with open("reformatted_source_code.html", "w", encoding="utf-8") as f:
-    f.write(text)
-
-# Il extrait les URLs en utilisant une expression rationnelle
+# Extrait les URLs en utilisant une expression rationnelle
 urls = re.findall(r'https://filemoon\.sx/e/[\w-]+/[\w\d\._-]+', text)
 
-# Il supprime les doublons de la liste
+# Supprime les doublons de la liste
 urls = list(set(urls))
 
-# Il extrait les numeros d'episodes et reformater les URL
+# Extrait les numéros d'épisodes et reformate les URLs
 episodes = []
 for url in urls:
     parts = url.split("/")
@@ -44,24 +32,21 @@ for url in urls:
         episode = f"Episode {int(episode_number):03d} : {stripped_url}"
         episodes.append(episode)
 
+# Trie les épisodes par numéro d'épisode
+episodes.sort(key=lambda x: int(x.split(" ")[1]))
 
-# Il trie les episodes par numero d'episode
-episodes.sort()
-
-# Il enleve le premier "0" dans les episodes nommes "Episode 010" a "Episode 099" et le premier "00" dans les episodes nommes "Episode 001" a "Episode 009"
+# Enlève le premier "0" dans les épisodes nommés "Episode 010" à "Episode 099" et le premier "00" dans les épisodes nommés "Episode 001" à "Episode 009"
 for i, episode in enumerate(episodes):
     parts = episode.split(" ")
     if parts[1].startswith("0"):
         parts[1] = str(int(parts[1]))
         episodes[i] = " ".join(parts)
 
-episodes.sort(key=lambda x: int(x.split(" ")[1])) 
-
-# Il ecrit les URLs extraites triees et reformatees dans un fichier
-with open("Episodes.txt", "w", encoding="utf-8") as f:
+# Écrit les URLs extraites triées et reformatées dans un fichier
+file_path = "1-Episodes.txt"
+with open(file_path, "w", encoding="utf-8") as f:
     for episode in episodes:
         f.write(episode + "\n\n")
 
-# Il supprime les fichiers de code source
-os.remove("source_code.html")
-os.remove("reformatted_source_code.html")
+    os.remove("source_code.html")
+    os.remove("reformatted_source_code.html")
